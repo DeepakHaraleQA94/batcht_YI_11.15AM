f804efceb7109678cb8f553f8d8786441bf921ec
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-06-17 11:54:42 +0530

    new repo

---
+++ src/main/java/SimpleWebScript/Automationscript.java
@@ -0,0 +1,15 @@
+package SimpleWebScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automationscript {
+
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mvnrepository.com/");
+	}
+}
