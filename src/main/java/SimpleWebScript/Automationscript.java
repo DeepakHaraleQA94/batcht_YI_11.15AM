531424ebb7ca58239425fc37f1f892a9cd84c4aa
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-06-20 15:45:54 +0530

    hi

--- src/main/java/SimpleWebScript/Automationscript.java
+++ src/main/java/SimpleWebScript/Automationscript.java
@@ -2,11 +2,12 @@ package SimpleWebScript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Automationscript {
 
-	
-	public static void main(String[] args) {
+	@Test
+	public void launch() {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
